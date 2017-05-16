f6ad5f24ff307e3afe4d107ae575087ebeb07393
jason <jf126dolphin@gmail.com>
2017-05-16 12:07:47 -0700

    Make webserver robust in using different PORTS, and print IP and portnumber

--- py/webserver.py
+++ py/webserver.py
@@ -12,17 +12,39 @@ except ImportError:
     import socketserver as SocketServer
 
 #No handling of bad arguments; will be called in shell script with proper public directory arg
-os.chdir(sys.argv[1])
+if len(sys.argv) != 2:
+    print("Usage: python webserver.py <directory_to_server>")
+    print("Error: expected 1 argument, got %d" % (len(sys.argv) - 1))
+    exit(1)
 
+os.chdir(sys.argv[1])
 PORT = 8080
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 SocketServer.TCPServer.allow_reuse_address = True # Allow reuse of port after ^C
-server = SocketServer.TCPServer(("", PORT), Handler)
 
-print("Web server serving: " + os.path.abspath(sys.argv[1]))
+server = None
+while True:
+    try:
+        server = SocketServer.TCPServer(("", PORT), Handler)
+        break
+    except:
+        PORT = PORT + 1
+
+
+print("==============================")
+print("Web server serving: %s on port %d" % (os.path.abspath(sys.argv[1]), PORT))
+import socket
+tempSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tempSocket.connect(("8.8.8.8", 80))
+IP = tempSocket.getsockname()[0]
+tempSocket.close()
+print("To view web interface, navigate to: %s:%d" %(IP, PORT))
+print("Note: Check IP address configuration if program is being run from a VM or a Docker Container.")
+print("==============================")
 
 try:
     server.serve_forever()
+
 except:
     print("Web server shutting down")
     server.shutdown()
