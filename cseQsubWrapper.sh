ae3cb8b10029c5158dabcbd3337ebc13dce9f8d4
jason <jf126dolphin@gmail.com>
2017-06-02 14:05:19 -0700

    Remove lab specific TMP from cseQsubWrapper

--- cseQsubWrapper.sh
+++ cseQsubWrapper.sh
@@ -6,10 +6,6 @@
 #$ -l h_rt=11:00:00
 #$ -N BrainSuite_QC
 
-#Nodes do not have enough TMP space for Cerebro, so use local temp
-#TODO: make this a parameter in subjects.sh (/ifs/tmp/ is specific to this lab)
-export TMPDIR=/ifs/tmp/
-export TMP=/ifs/tmp/
 
 python $1/py/cse.py $2 $3 $4 $5 $6 $7
 
